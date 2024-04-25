d08acf76801913e4df61dcc8c216c81c1f97cb8e
trinahombre <168095443+trinahombre@users.noreply.github.com>
2024-04-25 21:52:06 +0800

    website

---
+++ website/src/pages/Doctors.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import DoctorCard from "../components/DoctorCard";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import "../styles/doctors.css";
+import fetchData from "../helper/apiCall";
+import Loading from "../components/Loading";
+import { setLoading } from "../redux/reducers/rootSlice";
+import Empty from "../components/Empty";
+import { useAppDispatch, useAppSelector } from "../redux/store";
+
+const Doctors = () => {
+  const [doctors, setDoctors] = useState([]);
+  const dispatch = useAppDispatch();
+  const { loading } = useAppSelector((state) => state.root);
+
+  const fetchAllDocs = async () => {
+    dispatch(setLoading(true));
+    const data = await fetchData(`/doctor/getalldoctors`);
+    setDoctors(data);
+    dispatch(setLoading(false));
+  };
+
+  useEffect(() => {
+    fetchAllDocs();
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      {loading && <Loading />}
+      {!loading && (
+        <section className="container doctors">
+          <h2 className="page-heading">Our Doctors</h2>
+          {doctors.length > 0 ? (
+            <div className="doctors-card-container">
+              {doctors.map((ele) => {
+                return (
+                  <DoctorCard
+                    ele={ele}
+                    key={ele._id}
+                  />
+                );
+              })}
+            </div>
+          ) : (
+            <Empty />
+          )}
+        </section>
+      )}
+      <Footer />
+    </>
+  );
+};
+
+export default Doctors;
